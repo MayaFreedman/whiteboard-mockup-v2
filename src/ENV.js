5dd50a057f5e0e62735bb5add9af57a9c03d3c98
Maya Freedman <maya@playspace.health>
2025-08-11 18:01:39 -0400

    Added lovable preview link to the ENV.js file

--- src/ENV.js
+++ src/ENV.js
@@ -1,20 +1,22 @@
 export const ENV = {
   URL: {
-    'app-dev.practicespace.health': 'https://ca-yto-8b3f79b2.colyseus.cloud',
-    'app-dev.playspace.health': 'https://ca-yto-8b3f79b2.colyseus.cloud',
-    'app-staging.playspace.health': 'https://ca-yto-8b3f79b2.colyseus.cloud',
-    'app.practicespace.health': 'https://ca-yto-dd1ed7a2.colyseus.cloud',
-    'app.playspace.health': 'https://ca-yto-dd1ed7a2.colyseus.cloud',
-    localhost: 'http://localhost:4001',
+    "app-dev.practicespace.health": "https://ca-yto-8b3f79b2.colyseus.cloud",
+    "https://preview--whiteboard-mockup-v2.lovable.app":
+      "https://ca-yto-8b3f79b2.colyseus.cloud",
+    "app-dev.playspace.health": "https://ca-yto-8b3f79b2.colyseus.cloud",
+    "app-staging.playspace.health": "https://ca-yto-8b3f79b2.colyseus.cloud",
+    "app.practicespace.health": "https://ca-yto-dd1ed7a2.colyseus.cloud",
+    "app.playspace.health": "https://ca-yto-dd1ed7a2.colyseus.cloud",
+    localhost: "http://localhost:4001",
   },
   // DEFAULT_URL: 'https://ca-yto-8b3f79b2.colyseus.cloud', // TODO reset
-  DEFAULT_URL: 'http://localhost:4001',
+  DEFAULT_URL: "http://localhost:4001",
   STATE_VERSION: 1,
 };
 
 export function getConfigurationServerURL() {
   const domain = window.location.hostname;
-  console.log('[worry-pet-game] domain', domain);
+  console.log("[worry-pet-game] domain", domain);
   let server = ENV.URL[domain];
   if (!server) {
     console.warn(
@@ -23,6 +25,6 @@ export function getConfigurationServerURL() {
     server = ENV.DEFAULT_URL;
   }
 
-  console.log('[worry-pet-game] colyseus server ', server);
+  console.log("[worry-pet-game] colyseus server ", server);
   return server;
 }
